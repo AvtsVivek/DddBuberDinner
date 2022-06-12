07c402537d505d4ed5c004735e1898ea5aa280c6
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-12 17:14:51 +0530

    Refactored the files

--- src/BuberDinner/BuberDinner.Api/Program.cs
+++ src/BuberDinner/BuberDinner.Api/Program.cs
@@ -8,18 +8,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    // app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
